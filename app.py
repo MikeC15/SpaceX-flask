a6657d0801d3dff5d6763a7f063aabcda69dfce4
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-11-19 12:52:51 -0700

    users resource created

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify, g
+from flask_cors import CORS
+from resources.users import user
+import models
+
 
 DEBUG = True
 PORT = 8000
 
-# Initialize an instance of the Flask class.
-# This starts the website!
 app = Flask(__name__)
 
-# The default URL ends in / ("my-website.com/").
-@app.route('/')
-def index():
-    return 'hii'
+@app.before_request
+def before_request():
+    """Connect to the database before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
 
-# Run the app when the program starts!
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+
+CORS(user, origins=['http://localhost:3000'], supports_credentials=True)
+app.register_blueprint(user, url_prefix='/api/v1/users')
+
+# @app.route('/')
+# def index():
+#     return 'hii'
+
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
